150c687e8fed89f820dc631290488cfe24325cf6
Filip Drzazga <filip.drzazga@gmail.com>
2026-03-15 21:27:11 +0000

    new btn colors for print selection, ui

--- src/ui/components/Badge/Badge.jsx
+++ src/ui/components/Badge/Badge.jsx
@@ -1,34 +1,36 @@
 import style from "./Badge.module.css";
 const badgeArr = [
-    { type: 'Linear Meter', bgColor: '#f1f5f9', textColor: '#334155'},
-    { type: 'Fat Quarter', bgColor: '#f1f5f9', textColor: '#334155' },
-    { type: 'Cushion', bgColor: '#f1f5f9', textColor: '#334155'},
-    { type: 'Sample', bgColor: '#f1f5f9', textColor: '#334155' },
-    { type: 'Tea Towel', bgColor: '#f1f5f9', textColor: '#334155' },
-    { type: 'Polyesters', bgColor: '#eef2ff', textColor: '#4338ca' },
-    { type: 'Cottons', bgColor: '#ecfeff', textColor: '#0e7490' },
-    { type: 'READY', bgColor: '#ecfdf5', textColor: '#047857' },
-    { type: 'INVALID', bgColor: '#fffbeb', textColor: '#b45309' },
-    { type: 'UNKNOWN', bgColor: '#fff7ed', textColor: '#c2410c' },
-    { type: 'DiffDays', bgColor: '#ffffff', textColor: '#171717' },
-
-
+  { type: "Linear Meter", bgColor: "#f1f5f9", textColor: "#334155" },
+  { type: "Fat Quarter", bgColor: "#f1f5f9", textColor: "#334155" },
+  { type: "Cushion", bgColor: "#f1f5f9", textColor: "#334155" },
+  { type: "Sample", bgColor: "#f1f5f9", textColor: "#334155" },
+  { type: "Tea Towel", bgColor: "#f1f5f9", textColor: "#334155" },
+  { type: "Polyesters", bgColor: "#eef2ff", textColor: "#4338ca" },
+  { type: "Cottons", bgColor: "#ecfeff", textColor: "#0e7490" },
+  { type: "READY", bgColor: "#ecfdf5", textColor: "#61d041" },
+  { type: "INVALID", bgColor: "#fffbeb", textColor: "#b45309" },
+  { type: "UNKNOWN", bgColor: "#fff7ed", textColor: "#c2410c" },
+  { type: "DiffDays", bgColor: "#ffffff", textColor: "#171717" },
 ];
 
 const Badge = ({ type, badgeText }) => {
-    return (
-        <div className={style.badge_container}>
-{            badgeArr.map((badge)=>{
-                if(badge.type === type){
-                    return (
-                        <div key={badge.type} className={style.badge} style={{ backgroundColor: badge.bgColor, color: badge.textColor }}>
-                            {badgeText.toUpperCase()}
-                        </div>
-                    )
-                }
-            })}
-        </div>
-    )
+  return (
+    <div className={style.badge_container}>
+      {badgeArr.map((badge) => {
+        if (badge.type === type) {
+          return (
+            <div
+              key={badge.type}
+              className={style.badge}
+              style={{ backgroundColor: badge.bgColor, color: badge.textColor }}
+            >
+              {badgeText.toUpperCase()}
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
 };
 
 export default Badge;
